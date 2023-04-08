ab7ef3410dd8b5863c34ed88064714b2ea836b78
Anand <raschoudhary879@gmail.com>
2023-04-08 16:25:56 +0530

    Ad check

--- script/home.js
+++ script/home.js
@@ -12,3 +12,8 @@ const changeTheme=(n)=>{
         document.getElementById("signImg1").style.display = "block";
     }
 }
+window.onload = function () {
+    setTimeout(function () {
+        document.getElementById("myVideo").play();
+    }, 1000);
+};
